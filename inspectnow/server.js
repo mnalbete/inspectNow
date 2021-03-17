6a89e77480d94468b207d208f9ecb2d8843d032e
mathew <mnalbete@uw.edu>
2021-03-16 21:43:44 -0700

    added mongoose server.js update

--- inspectnow/server.js
+++ inspectnow/server.js
@@ -4,6 +4,9 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD
+);
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -19,10 +22,15 @@ app.use(routes);
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/properties";
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI, DB {
   useUnifiedTopology: true,
-  useNewUrlParser: true
-});
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  }).then(con => {
+    console.log(con.connects);
+    console.log('DB connection sucessful!')
+  });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
