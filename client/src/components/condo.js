7c4c650f65daa3380f168c10ee3ff51d20123860
Orlando <baello2020@gmail.com>
2021-03-20 10:34:56 -0700

    condo.js updated

--- client/src/components/condo.js
+++ client/src/components/condo.js
@@ -32,7 +32,6 @@ function Condo() {
             <h1>Property INFO&nbsp;&nbsp;</h1>
         <input type="email" class="form-control" id="info" aria-describedby="emailHelp" placeholder="Type Adress Here"></input>
         <br/>
-       
         <br/>
         <div htmlFor="info" id="info">
         This confidential report is prepared exclusively for:
@@ -63,46 +62,66 @@ function Condo() {
             </section>
           <section id="section-3">
             <h1>Frontdoor&nbsp;&nbsp;</h1>
-            <input type="checkbox" name="test" ripple defaultChecked/>test&nbsp;&nbsp;
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>peephole with knocker&nbsp;&nbsp;
             <br></br>
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>Swing&nbsp;&nbsp;
             <br></br>
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>Latching&nbsp;&nbsp;
             <br></br>
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>handle&nbsp;&nbsp;
             <br></br>
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>Scratches&nbsp;&nbsp;
             <br></br>
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>Door stop&nbsp;&nbsp;
             <br></br>
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>Self-closing Hinges&nbsp;&nbsp;
             <br></br>
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>Lights above front door&nbsp;&nbsp;
             <br></br>
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
-            <br></br>
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
-            <br></br>
-            <input type="checkbox" name="test"/>test&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>Wall around front door&nbsp;&nbsp;
             </section>
           <section id="section-4">
             <h1>HVAC&nbsp;&nbsp;</h1>
+            <input type="checkbox" name="test"/>Replace Filter&nbsp;&nbsp;
+            <br></br>
+            <input type="checkbox" name="test"/>Thermostat Setting&nbsp;&nbsp;
+            <br></br>
+            <input type="checkbox" name="test"/>heating&nbsp;&nbsp;
+            <br></br>
+            <input type="checkbox" name="test"/>cooling&nbsp;&nbsp;
+            <br></br>
+            <input type="checkbox" name="test"/>register alignment&nbsp;&nbsp;
+            <br></br>
             </section>
           <section id="section-5">
             <h1>Ceiling&nbsp;&nbsp;</h1>
+            <input type="checkbox" name="test"/>Paint&nbsp;&nbsp;
+            <br></br>
+            <input type="checkbox" name="test"/>Molding&nbsp;&nbsp;
+            <br></br>
+            <input type="checkbox" name="test"/>Diffuser alignment&nbsp;&nbsp;
+            <br></br>
+            <input type="checkbox" name="test"/>Sprinklers&nbsp;&nbsp;
+            <br></br>
+            <input type="checkbox" name="test"/>Smoke detectors&nbsp;&nbsp;
             </section>
           <section id="section-6">
             <h1>Kitchen&nbsp;&nbsp;</h1>
+            <h3>Kitchen / Outlets&nbsp;&nbsp;</h3>
+            <input type="checkbox" name="test"/>Ceiling lights&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>Under Cabinet lights&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>GFCI&nbsp;&nbsp;
+            <h3>Dishwasher&nbsp;&nbsp;</h3>
+            <input type="checkbox" name="test"/>Air Gap&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>operational&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>DHigh loop&nbsp;&nbsp;
+            <h3>Microwave&nbsp;&nbsp;</h3>
+            <input type="checkbox" name="test"/>clean&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>Heating&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>Time set&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>filter&nbsp;&nbsp;
+            <input type="checkbox" name="test"/>lighting&nbsp;&nbsp;
+   
             </section>
           <section id="section-7">
             <h1>Bathroom&nbsp;&nbsp;</h1>
