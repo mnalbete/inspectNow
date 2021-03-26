49e1da396498dd75a077312bd6607508eed7603b
Ryan <ryan@Ryans-iMac.local>
2021-03-26 14:03:52 -0700

    Updated Reg Login and Body Components

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -2,10 +2,10 @@ import api from '../utils/api';
 import {useAuthenticatedUser} from '../utils/auth';
 import { useHistory } from 'react-router-dom';
 
-
 function Body() {
   const user = useAuthenticatedUser();
   const history = useHistory();
+  console.log(user);
   const handleSubmit = async e => {
     e.preventDefault();
 
@@ -13,12 +13,10 @@ function Body() {
 
     try {
 
-      let property = await api.createProperty({userId : user._id});
-      let array = property.data.properties;
-      let len = property.data.properties.length
-      console.log(property.data.properties[len -1]);
-      let currentPropertyId = property.data.properties[len -1];
-      const path = `/api/properties/` + currentPropertyId;
+      let property = await api.createProperty({ userId: user._id });
+
+      console.log(property.data._id);
+      const path = `/api/properties/` + property.data._id;
       history.push(path);
       // User has been successfully logged in and added to state. Perform any additional actions you need here such as redirecting to a new page.
 
@@ -35,7 +33,7 @@ function Body() {
 
 
     try {
-      const path = `/properties`;
+      const path = `/api/properties/`;
       history.push(path);
       // User has been successfully logged in and added to state. Perform any additional actions you need here such as redirecting to a new page.
 
@@ -47,31 +45,43 @@ function Body() {
     }
   }
 
+// Updated CSS
     return (
-      <div id = "back" styles={{ backgroundImage: "./images/background.png" }}> 
-<div class="row">
-  <div class="col-sm-2">
-    <div class="card text-center">
-    <img src="/images/logo.png" class="card-img-top" alt="..."></img>
-      <div class="card-body">
-        <a href="#" class="btn btn-outline-success" onClick={handleSubmitList}>View Reports</a>
-      </div>
-    </div>
+      <div className = "backgroundHome"  style={{ 
+        backgroundImage: `url(/images/pexels-binyamin-mellish-106399.jpg)`,   backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat', height: `1960px`
+      }}>
+  
+        <div class = "home" >
+  <div >
+  {/* <div class="col-sm-2"> */}
+    {/* <div class="card text-center"> */}
+    {/* <img src="/images/logo.png" class="card-img-top" alt="..."></img> */}
+      {/* <div class="card-body"> */}
+        <a href="#" class="btn btn-success btn-block mt-3" onClick={handleSubmitList}>View Reports</a>
+      {/* </div>
+    </div> */}
   </div>
-  <div class="col-sm-2">
-    <div class="card text-center">
-    <img src="/images/logo.png" class="card-img-top" alt="..."></img>
-      <div class="card-body">
-        <a href="#" class="btn btn-outline-success" onClick={handleSubmit}>Inspect Property</a>
-
-
+  {/* <div class="col-sm-2"> */}
+    {/* <div class="card text-center"> */}
+    {/* <img src="/images/logo.png" class="card-img-top" alt="..."></img> */}
+      {/* <div class="card-body"> */}
+        <a href="#" class="btn btn-success btn-block mt-3" 
+       onClick={handleSubmit}>Inspect Property</a>
+  
+  
       </div>
-    </div>
-  </div>
-</div>
-
-</div>
+  
       
-    );
-  }
+         </div>
+    )}
+
+
+
+
+
+
+
+  
+
   export default Body;
--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -3,6 +3,7 @@ import {useLogin} from '../utils/auth';
 import api from '../utils/api';
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom'
+import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
 function RegistrationForm() {
 
@@ -40,19 +41,41 @@ function RegistrationForm() {
         }
     }
 
-    return (
-        <body style={{ backgroundImage: "url(/images/background.png)", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
-            <div class="row justify-content-md-center vh-100">
-                <form onSubmit={handleSubmit}>
-                    <h2>Register</h2>
-                    <input type="text" ref={emailRef} placeholder="Your email" /><br />
-                    <input type="password" ref={passwordRef} placeholder="Your password" /><br />
-                    <button className="btn btn-success" >Register</button>
-                </form>
-                <Link class= "text-success" to="/login"> Already a member? Login here. </Link>
-            </div>
-        </body>
-    )
+//     return (
+//         <body style={{ backgroundImage: "url(/images/background.png)", backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
+//             <div class="row justify-content-md-center vh-100">
+//                 <form onSubmit={handleSubmit}>
+//                     <h2>Register</h2>
+//                     <input type="text" ref={emailRef} placeholder="Your email" /><br />
+//                     <input type="password" ref={passwordRef} placeholder="Your password" /><br />
+//                     <button className="btn btn-success" >Register</button>
+//                 </form>
+//                 <Link class= "text-success" to="/login"> Already a member? Login here. </Link>
+//             </div>
+//         </body>
+//     )
 
+// }
+
+
+return (
+         
+    <body style={{ backgroundImage: `url(/images/frontDoor.jpg)`, backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat', height: `1960px`, width: `100%` }}>
+    <form id = "myForm">
+      <div className="form-group" style={{ marginRight:`10%`,marginLeft:`10%` }}>
+        <label htmlFor="exampleInputEmail1" style={{ color:`white` }}>Email address</label>
+        <input type="text" className="form-control" id="exampleInputEmail1" ref={emailRef} placeholder="Enter email"  />
+        {/* /* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+      </div> */
+      <div className="form-group" style={{ marginRight:`10%`,marginLeft:`10%` }}>
+        <label htmlFor="exampleInputPassword1" style={{ color:`white` }}>Password</label>
+        <input type="password" className="form-control" id="exampleInputPassword1" ref={passwordRef} placeholder="Password"  />
+      </div>
+      <button type="submit" className="btn btn-success btn-lg mt-4" style={{width: `80%`,marginRight:`10%`,marginLeft:`10%`}}>Submit</button>
+    </form>
+    <Link className= "text-success " to="/register" style={{ color:`white` }}> Not a member? Register here. </Link>
+    </body>
+  );
 }
 export default RegistrationForm;
