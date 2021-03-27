2f26d72d2f8f1afde77578b4e5265df7baa32155
X.LI <ralflea@gmail.com>
2021-03-27 12:34:57 -0700

    revert findall

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -3,7 +3,7 @@ const db = require("../models");
 module.exports = {
     findAll: function(req, res) {
       db.Property
-      .findById({ _id: req.params.id })
+      .findById({userId: req.user._id})
       .then(dbProp => { res.json(dbProp);})
       .catch(err => { res.json(err);})
     },
