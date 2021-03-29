e01f1fdde4dfc29a75bb299a8f77923dc14cf693
mathew <mnalbete@uw.edu>
2021-03-28 22:00:05 -0700

    changed the condo.js forms to save section one data

--- models/property_info.js
+++ models/property_info.js
@@ -4,6 +4,10 @@ const Image = require("./image")
 const property_infoSchema = new Schema({
     id: {type: String, require: true},
     address: {type: String, require: true},
+    homeowner: {type: String, require: true},
+    inspector: {type: String, require: true},
+    reportnumber: {type: String, require: true},
+    frontdoorcomment: {type: String, require: true},
     inspectionDate: {type: String},
     realEstateAgentName: {type: String},
     inspectorInfo: {type: String},
